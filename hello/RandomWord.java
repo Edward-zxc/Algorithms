c5aa2f00703f23d59043c500ab5d0cdc6219304b
Edward xc <3313338649@qq.com>
2024-05-14 18:36:01 +0800

    week1

--- hello/RandomWord.java
+++ hello/RandomWord.java
@@ -1,5 +1,23 @@
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 public class RandomWord {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+  public static void main(String[] args) {
+    if (StdIn.isEmpty()) {
+            StdOut.println("No words entered");
+        }
+    else {
+            String champion =StdIn.readString();
+        int i = 0,len= args.length;
+        while (i < len) {
+            if (StdRandom.bernoulli(1.0/len)) {
+                champion = args[i];
+            }
+            i++;
+        }
+        StdOut.println(champion);
+        }
     }
 }
